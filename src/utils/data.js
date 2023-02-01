a98693e7f130bee643e04890097239920266d5c4
Caslav Lazic <caslavlazic@gmail.com>
2023-02-01 18:30:20 +0100

    Making data

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,7 +3,38 @@ const data = [
     id: 1,
     home: 'Brasil',
     away: 'Urugway',
-    score: [0, 0]
-  }
+    score: [3, 3]
+  },
+  {
+    id: 2,
+    home: 'Paragway',
+    away: 'Chile',
+    score: [2, 3]
+  },
+  {
+    id: 3,
+    home: 'Ecuador',
+    away: 'Bolivia',
+    score: [2, 4]
+  },
+  {
+    id: 4,
+    home: 'Guatemala',
+    away: 'Honduras',
+    score: [5, 0]
+  },
+  {
+    id: 5,
+    home: 'Columbia',
+    away: 'Venezuela',
+    score: [6, 1]
+  },
+  {
+    id: 6,
+    home: 'Mexico',
+    away: 'Belize',
+    score: [8, 0]
+  },
+
 ]
 
